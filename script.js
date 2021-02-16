33715b95f64af3c43023f01656f96b94986af954
Emilio Artigas <59299705+emilioartigas@users.noreply.github.com>
2021-02-15 19:37:30 -0600

    jajajajaja

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+var a1 = document.getElementById(a1);
+var a2 = document.getElementById(a2);
+var a3 = document.getElementById(a3);
+var b1 = document.getElementById(b1);
+var b2 = document.getElementById(b2);
+var b3 = document.getElementById(b3);
+var c1 = document.getElementById(c1);
+var c2 = document.getElementById(c2);
+var c3 = document.getElementById(c3);
+
+var board = [
+  [a1, a2, a3],
+  [b1, b2, b3],
+  [c1, c2, c3],
+];
+
+function changeBackground(currentPlayer){
+  if(currentPlayer % 2 === 0){
+    a1.style.backgroundImage = "url(images/x.png)"
+  }
+  else{
+    a1.style.backgroundImage = "url(images/o.png)"
+  }
+}
 function randomPick(emptySpaces){
     return Math.floor(Math.random() * (emptySpaces+1)); // random number between 0 and emptySpaces
 }
@@ -5,5 +29,3 @@ function randomPick(emptySpaces){
 function whoGoesFirst(){
     return Math.floor(Math.random() * 2); // random number between 0 and 1
 }
-
-
